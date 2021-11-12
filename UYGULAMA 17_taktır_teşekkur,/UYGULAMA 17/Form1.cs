33a72afb8ee78cbc396fceed2ad1980f7efde189
kirpanarda <kirpanarda@gmail.com>
2021-11-12 14:05:55 +0300

    .

---
+++ UYGULAMA 17_taktır_teşekkur,/UYGULAMA 17/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UYGULAMA_17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int not = Convert.ToInt32(textBox1.Text);
+            if (not >=85)
+            {
+                MessageBox.Show("takdir ile geçtiniz...");
+            }
+            else if (not >=70)
+            {
+                MessageBox.Show("teşekkür ile geçtiniz...");
+            }
+            else
+            {
+                MessageBox.Show("belge almadan sınıf geçtiniz...");
+            }
+          
+        }
+       
+        
+    }
+
+}
